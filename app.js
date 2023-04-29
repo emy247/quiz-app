ded281a6f8fad2e404928b95c849d1e5fc8aa9da
emy247 <danyb0@yahoo.com>
2023-04-29 22:45:23 +0300

    Final update

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const { connectToDb, getDb } = require('./db');
-
 
 connectToDb((err) => {
   if (err) {
@@ -59,65 +57,3 @@ app.get('/javascript', (req, res) => {
 
 
 
-=======
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const { connectToDb, getDb } = require('./db');
-
-
-connectToDb((err) => {
-  if (err) {
-    console.log('Error connecting to database', err);
-  } else {
-    app.listen(5000, () => {
-      console.log('App listening on port 5000');
-    });
-  }
-});
-
-app.use(cors());
-
-app.get('/html', (req, res) => {
-  const db = getDb();
-
-  db.collection('html').find({}).toArray()
-    .then((books) => {
-      res.status(200).json(books);
-      date = books;
-    })
-    .catch(() => {
-      res.status(500).json({ error: 'could not fetch' });
-    });
-});
-
-app.get('/css', (req, res) => {
-  const db = getDb();
-
-  db.collection('css').find({}).toArray()
-    .then((books) => {
-      res.status(200).json(books);
-      date = books;
-    })
-    .catch(() => {
-      res.status(500).json({ error: 'could not fetch' });
-    });
-});
-
-app.get('/javascript', (req, res) => {
-  const db = getDb();
-
-  db.collection('javascript').find({}).toArray()
-    .then((books) => {
-      res.status(200).json(books);
-      date = books;
-    })
-    .catch(() => {
-      res.status(500).json({ error: 'could not fetch' });
-    });
-});
-
-
-
-
->>>>>>> 1d10ca396885ad4994d694ef47db0ed740e9a3c0
--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { MongoClient } = require('mongodb');
 
 let dbConnection
@@ -14,26 +13,7 @@ module.exports={
             console.log(err)
 
         })
-    },
+    }, 
+    
     getDb:()=>dbConnection
 }
-=======
-const { MongoClient } = require('mongodb');
-
-let dbConnection
-
-module.exports={
-    connectToDb:(cb)=>{
-        MongoClient.connect('mongodb+srv://quizdata:U23bjK8mOXgvSJil@quiz.wmufbkq.mongodb.net/?retryWrites=true&w=majority')
-        .then((client)=>{
-            dbConnection=client.db('htmldb')
-            return cb()
-        })
-        .catch(err=>{
-            console.log(err)
-
-        })
-    },
-    getDb:()=>dbConnection
-}
->>>>>>> 1d10ca396885ad4994d694ef47db0ed740e9a3c0
