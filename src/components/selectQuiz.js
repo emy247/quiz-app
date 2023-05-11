c6f2bd59e7aadf22041074999733389fec55b9b9
emy247 <danyb0@yahoo.com>
2023-05-11 03:03:38 +0300

    quiz select update

--- src/components/selectQuiz.js
+++ src/components/selectQuiz.js
@@ -14,10 +14,10 @@ const Home = () => {
 
 
   let selectQuizButtons = [
-    { id: '1', label: 'JavaScript', quiz:'html', status:'Uncompleted'},
-    { id: '2', label: 'HTML', quiz:'css', status:'Uncompleted'},
-    { id: '3', label: 'CSS' , quiz:'javascript', status:'Uncompleted'},
-    { id: '4', label: 'React' , quiz:'react', status:'Uncompleted'},
+    { id: '1', label: 'HTML', quiz:'html', status:'Uncompleted'},
+    { id: '2', label: 'CSS', quiz:'css', status:'Uncompleted'},
+    { id: '3', label: 'JavaScript' , quiz:'javascript', status:'Uncompleted'},
+    { id: '4', label: 'React.js' , quiz:'react', status:'Uncompleted'},
     { id: '5', label: 'Node.js' , quiz:'node', status:'Uncompleted'},
     { id: '6', label: 'Data structures' , quiz:'data', status:'Uncompleted'},
     { id: '7', label: 'Algorithms' , quiz:'algorithms', status:'Uncompleted'},
